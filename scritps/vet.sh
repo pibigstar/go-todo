ce4c40350a384466cdf1859e6e629e281b4b324b
pibigstar <741047261@qq.com>
2019-06-19 10:57:12 +0800

    去掉goimport检查

--- scritps/vet.sh
+++ scritps/vet.sh
@@ -4,6 +4,6 @@ cd "${GOPATH}/src/github.com/pibigstar/go-todo"
 
 find . -name "*.go" -not -path "./third/*" | xargs gofmt -w
 
-find . -name "*.go" -not -path "./third/*" | xargs goimports -w
+# find . -name "*.go" -not -path "./third/*" | xargs goimports -w
 
 git diff --exit-code
