2f453dc1fab7b35c50699950457f288953e772f4
pibigstar <741047261@qq.com>
2019-06-19 10:53:13 +0800

    精简代码

--- scritps/vet.sh
+++ scritps/vet.sh
@@ -2,8 +2,6 @@
 
 cd "${GOPATH}/src/github.com/pibigstar/go-todo"
 
-find . -type f -not -path "./third/*" | xargs -0 misspell -error
-
 find . -name "*.go" -not -path "./third/*" | xargs gofmt -w
 
 find . -name "*.go" -not -path "./third/*" | xargs goimports -w
